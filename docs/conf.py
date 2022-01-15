03f4232dea8f55e21fd4b4b61623af6431c8f06f
Veit Schiele <kontakt@veit-schiele.de>
2022-01-15 17:33:03 +0100

    Fix svg errors by building latexpdf

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.graphviz',
     'sphinx.ext.todo',
+    "sphinxcontrib.rsvgconverter"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
