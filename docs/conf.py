6e500c690cd7c47e2ccc9b65cb68f3fe84a47a19
Veit Schiele <kontakt@veit-schiele.de>
2022-04-03 13:52:16 +0200

    :wrench: Add Sphinx Copybutton to Sphinx requirements

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ extensions = [
     'sphinx.ext.graphviz',
     'sphinx.ext.todo',
     "sphinxcontrib.rsvgconverter",
-    "sphinxext.opengraph"
+    "sphinxext.opengraph",
+    "sphinx_copybutton"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
